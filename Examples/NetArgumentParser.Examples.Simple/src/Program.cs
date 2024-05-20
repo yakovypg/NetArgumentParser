d0216ecd69617d0ed2343650a6923266f59bbc32
yakovypg <yasha-pmp@mail.ru>
2024-05-20 21:01:08 +0300

    Add simple example

---
+++ Examples/NetArgumentParser.Examples.Simple/src/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using NetArgumentParser;
+using NetArgumentParser.Options;
+using NetArgumentParser.Options.Context;
+
+bool showHelp = false;
+int? angle = default;
+FileMode? fileMode = default;
+List<string> inputFiles = [];
+
+var options = new ICommonOption[]
+{   
+    new HelpOption(afterHandlingAction: () => showHelp = true),
+
+    new MultipleValueOption<string>("input", "i",
+        description: "images that need to be processed",
+        isRequired: true,
+        contextCapture: new OneOrMoreContextCapture(),
+        afterValueParsingAction: t => inputFiles = new List<string>(t)),
+
+    new ValueOption<int>("angle", "a",
+        description: "angle by which you want to rotate the image",
+        isRequired: true,
+        afterValueParsingAction: t => angle = t),
+    
+    new EnumValueOption<FileMode>("file-mode", string.Empty,
+        description: "specifies how the operatng system should open a file",
+        defaultValue: new DefaultOptionValue<FileMode>(FileMode.OpenOrCreate),
+        afterValueParsingAction: t => fileMode = t)
+};
+
+var parser = new ArgumentParser();
+parser.AddOptions(options);
+
+try
+{
+    parser.Parse(args);
+}
+catch (Exception ex)
+{
+    Console.WriteLine($"Error: {ex.Message}");
+    return;
+}
+
+if (showHelp)
+{
+    Console.WriteLine(parser.GenerateProgramDescription());
+    return;
+}
+
+Console.WriteLine($"Help: {showHelp}");
+Console.WriteLine($"Angle: {angle}");
+Console.WriteLine($"File mode: {fileMode}");
+Console.WriteLine($"Input files: {string.Join(' ', inputFiles)}");
