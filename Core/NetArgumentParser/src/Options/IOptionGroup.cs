90c691a4d0cffb4d119ca559654fd79c322ac880
yakovypg <yasha-pmp@mail.ru>
2025-01-27 11:29:48 +0300

    Add header and description to mutually exclusive option group

---
+++ Core/NetArgumentParser/src/Options/IOptionGroup.cs
@@ -0,0 +1,14 @@
+using System.Collections.Generic;
+
+namespace NetArgumentParser.Options;
+
+public interface IOptionGroup<T>
+    where T : IOption
+{
+    string Header { get; set; }
+    string Description { get; set; }
+    IReadOnlyList<T> Options { get; }
+
+    void AddOptions(params T[] options);
+    bool RemoveOption(T item);
+}
