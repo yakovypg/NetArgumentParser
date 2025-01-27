075c0d7ec8b508c0ff2f1a6e81ee2e20f569e322
yakovypg <yasha-pmp@mail.ru>
2025-01-27 11:52:33 +0300

    Disable CA2263 warning

--- Core/NetArgumentParser/src/Converters/EnumValueConverter.cs
+++ Core/NetArgumentParser/src/Converters/EnumValueConverter.cs
@@ -5,6 +5,8 @@ namespace NetArgumentParser.Converters;
 public class EnumValueConverter<T> : ValueConverter<T>
     where T : struct, Enum
 {
+#pragma warning disable CA2263 // Prefer generic overload when type is known
     public EnumValueConverter(bool ignoreCase = true)
         : base(t => (T)Enum.Parse(typeof(T), t, ignoreCase)) { }
+#pragma warning restore CA2263 // Prefer generic overload when type is known
 }
