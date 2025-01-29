6b8f9707ca0e0330fee079da8fa5f7d12f3f3b8a
yakovypg <yasha-pmp@mail.ru>
2025-01-29 18:19:15 +0300

    Add the ability to not specify the same header and description for same groups

--- Core/NetArgumentParser/src/Attributes/Extensions/PropertyInfoEnumerableExtensions.cs
+++ Core/NetArgumentParser/src/Attributes/Extensions/PropertyInfoEnumerableExtensions.cs
@@ -11,7 +11,37 @@ internal static class PropertyInfoEnumerableExtensions
     internal static IEnumerable<IGrouping<OptionGroupAttribute?, PropertyInfo>> GroupOptionAttributes(
         this IEnumerable<PropertyInfo> properties)
     {
+        ExtendedArgumentNullException.ThrowIfNull(properties, nameof(properties));
         return properties.GroupBy(t => t.GetCustomAttribute<OptionGroupAttribute>());
+    }
+
+    internal static T? FindOptionGroupAttributeWithBestParameters<T>(
+        this IEnumerable<PropertyInfo> properties,
+        string groupId)
+        where T : OptionGroupBaseAttribute
+    {
+        ExtendedArgumentNullException.ThrowIfNull(properties, nameof(properties));
+        ExtendedArgumentNullException.ThrowIfNull(groupId, nameof(groupId));
+
+        List<T> candidates = properties
+            .Select(t => t.GetCustomAttribute<T>())
+            .Where(t => t is not null && t.Id == groupId)
+            .ToList()!;
+
+        bool isGroupWithInfoExists = candidates.Any(t =>
+        {
+            return !string.IsNullOrEmpty(t.Header)
+                || !string.IsNullOrEmpty(t.Description);
+        });
+
+        if (!isGroupWithInfoExists)
+            return candidates.FirstOrDefault();
+
+        T? bestGroupByHeader = candidates
+            .FirstOrDefault(t => !string.IsNullOrEmpty(t.Header));
+
+        return bestGroupByHeader ?? candidates
+            .FirstOrDefault(t => !string.IsNullOrEmpty(t.Description));
     }
 
     internal static Dictionary<PropertyInfo, ICommonOption> CreateOptions(
