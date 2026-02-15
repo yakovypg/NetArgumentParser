ee817ea02ca51736efc95a0ee81b5dd1834c4e0c
yakovypg <yasha-pmp@mail.ru>
2026-02-15 16:18:42 +0300

    Add the ability to use the converter function for `MultipleValueConverter` that returns a collection

--- Core/NetArgumentParser/Converters/MultipleValueConverter.cs
+++ Core/NetArgumentParser/Converters/MultipleValueConverter.cs
@@ -7,4 +7,7 @@ public class MultipleValueConverter<T> : ValueConverter<IList<T>>
 {
     public MultipleValueConverter(Func<string, T> singleValueConverter)
         : base(t => [singleValueConverter(t)]) { }
+
+    public MultipleValueConverter(Func<string, IList<T>> multipleValueConverter)
+        : base(t => multipleValueConverter(t)) { }
 }
