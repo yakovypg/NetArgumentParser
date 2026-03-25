594a9ec3c7faac1784d81dcbd0029738d532f02d
yakovypg <yasha-pmp@mail.ru>
2026-03-25 18:33:00 +0300

    Fix string extensions

--- Core/NetArgumentParser/Extensions/StringExtensions.cs
+++ Core/NetArgumentParser/Extensions/StringExtensions.cs
@@ -1,32 +1,65 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
+using System.Text;
 
 namespace NetArgumentParser.Extensions;
 
 internal static class StringExtensions
 {
-#if !NET5_0_OR_GREATER && !NETCOREAPP3_0_OR_GREATER && !NETSTANDARD2_1_OR_GREATER
-    private const StringComparison _defaultComparisonType = StringComparison.CurrentCulture;
+    private const StringComparison _defaultComparisonType = StringComparison.Ordinal;
 
-#pragma warning disable IDE0060 // Remove unused parameter
+#if !NET5_0_OR_GREATER && !NETCOREAPP3_0_OR_GREATER && !NETSTANDARD2_1_OR_GREATER
+    internal static string Replace(
+        this string text,
+        string oldValue,
+        string? newValue,
+        StringComparison comparisonType = _defaultComparisonType)
+    {
+        ExtendedArgumentNullException.ThrowIfNull(text, nameof(text));
+        ExtendedArgumentException.ThrowIfNullOrEmpty(oldValue, nameof(oldValue));
+
+        string replacement = newValue ?? string.Empty;
+
+        int currentPosition = 0;
+        int matchIndex = text.IndexOf(oldValue, currentPosition, comparisonType);
+
+        if (matchIndex < 0)
+            return text;
+
+        var builder = new StringBuilder(text.Length);
+
+        while (matchIndex >= 0)
+        {
+            _ = builder
+                .Append(text, currentPosition, matchIndex - currentPosition)
+                .Append(replacement);
+
+            currentPosition = matchIndex + oldValue.Length;
+            matchIndex = text.IndexOf(oldValue, currentPosition, comparisonType);
+        }
+
+        return builder
+            .Append(text, currentPosition, text.Length - currentPosition)
+            .ToString();
+    }
+
     internal static bool Contains(
         this string text,
         char value,
         StringComparison comparisonType = _defaultComparisonType)
     {
-        ExtendedArgumentNullException.ThrowIfNull(text, nameof(text));
-        return text.Contains($"{value}");
+        return text.Contains($"{value}", comparisonType);
     }
-#pragma warning restore IDE0060 // Remove unused parameter
 
-    internal static bool StartsWith(
+    internal static bool Contains(
         this string text,
-        char value,
+        string? value,
         StringComparison comparisonType = _defaultComparisonType)
     {
         ExtendedArgumentNullException.ThrowIfNull(text, nameof(text));
-        return text.StartsWith($"{value}", comparisonType);
+
+        return value is not null
+            ? text.IndexOf(value, comparisonType) >= 0
+            : false;
     }
 
     internal static int IndexOf(
@@ -39,6 +72,24 @@ internal static class StringExtensions
     }
 #endif
 
+    internal static bool StartsWith(
+        this string text,
+        char value,
+        StringComparison comparisonType = _defaultComparisonType)
+    {
+        ExtendedArgumentNullException.ThrowIfNull(text, nameof(text));
+        return text.StartsWith($"{value}", comparisonType);
+    }
+
+    internal static bool EndsWith(
+        this string text,
+        char value,
+        StringComparison comparisonType = _defaultComparisonType)
+    {
+        ExtendedArgumentNullException.ThrowIfNull(text, nameof(text));
+        return text.EndsWith($"{value}", comparisonType);
+    }
+
     internal static string RemoveLineBreakFromEnd(this string text)
     {
         ExtendedArgumentNullException.ThrowIfNull(text, nameof(text));
@@ -49,7 +100,7 @@ internal static class StringExtensions
     {
         ExtendedArgumentNullException.ThrowIfNull(text, nameof(text));
 
-        while (text.EndsWith(lineBreak, StringComparison.CurrentCulture))
+        while (text.EndsWith(lineBreak, StringComparison.Ordinal))
         {
             int lineBreakLength = lineBreak.Length;
             text = text.Remove(text.Length - lineBreakLength);
